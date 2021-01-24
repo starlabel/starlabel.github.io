f64500f05f5ce63a0f29ceb16db8db8a53b91c05
Data Ng <data@connect.hku.hk>
2021-01-25 02:12:09 +0800

    update header and footer

---
+++ menu.js
@@ -0,0 +1,18 @@
+document.write(`
+<div data-collapse="medium" data-animation="default" data-duration="400" class="nav-bar w-nav">
+    <div class="wrapper navbar-2 w-container">
+      <div class="logo-area"><a href="#" class="nav-logo w-inline-block"><img src="images/duduk.svg" width="106" alt=""></a></div>
+	  <nav role="navigation" class="nav-menu-2 w-nav-menu">
+	  <a href="index.html" class="nav-link w-nav-link w--current ">Home</a>
+	  <a href="about-us.html" class="nav-link w-nav-link">About Us</a>
+	  <a href="Event-Management.html" class="nav-link w-nav-link">Event Management</a>
+	  <a href="Digital-Marketing.html" class="nav-link w-nav-link">Talents</a>
+	  <a href="Talents.html" class="nav-link w-nav-link">Digital Marketing</a>	  <a href="portfolio.html" class="nav-link w-nav-link">Work</a>
+        <div class="nav-cta-button-container"><a href="contact-us.html" class="nav-link-2 border cta w-nav-link">Contact Us</a></div>
+      </nav>
+      <div class="menu-button-2 w-nav-button">
+        <div class="burger-icon w-icon-nav-menu"></div>
+      </div>
+    </div>
+  </div>
+`);
