85f3e86d50796fe7e9b5f6b283730d000b3d4b5a
Data Ng <data@connect.hku.hk>
2021-01-25 04:57:09 +0800

    update about and event management

--- footer.js
+++ footer.js
@@ -2,58 +2,59 @@ document.write(`
 <div class="section section-footer">
     <div class="wrapper">
       <div class="row-2">
-        <div class="col-2 lg-3 md-4"><a href="#" data-ix="logo" class="footer-logo-2 w-nav-brand"><img src="images/duduk-white.svg" width="104" alt=""></a>
-          <p class="ondark">From empowering challenger brands to think big to helping global brands feel closer to home, we leverage technology to bring people closer to the products, content, and experiences they love.</p>
+        <div class="col-2 lg-3 md-4"><a href="#" data-ix="logo" class="footer-logo-2 w-nav-brand">
+        <img src="images/starLabel_white.svg" width="300" alt=""></a>
+          <p class="ondark">Star Label is a Hong Kong-registered event management and entertainment company.  With years of experience in organizing large-scale entertainment projects, brand promotion activities, concerts, TV commercials, press conferences and new launch event, etc.</p>
         </div>
-        <div class="col-2 lg-2 sm-6">
+
+        <!-- <div class="col-2 lg-2 sm-6">
           <div class="footer-links-container">
             <h5 class="footer-heading"><a href="#" class="no-underline">Our Work</a></h5>
             <a href="#" class="footer-link ondark">Portfolio</a>
             <a href="blog.html" class="footer-link ondark">Contact us</a>
             <a href="#" class="footer-link ondark">Careers</a></div>
-        </div>
+        </div> -->
+
         <div class="col-2 lg-2 sm-6">
           <div class="footer-links-container">
             <h5 class="footer-heading"><a href="#" class="no-underline">Company</a></h5>
             <a href="#" class="footer-link ondark">About Us</a>
-            <a href="#" class="footer-link ondark">Our Team</a>
-            <a href="#" class="footer-link ondark">Blog</a>
-            <a href="#" class="footer-link ondark">FAQs</a></div>
+            <a href="#" class="footer-link ondark">Our Partners </a>
+            <a href="#" class="footer-link ondark">Work </a>
+            </div>
         </div>
         <div class="col-2 lg-2 sm-6">
           <div class="footer-links-container">
-            <h5 class="footer-heading"><a href="#" class="no-underline">Business</a></h5>
-            <a href="#" class="footer-link ondark">Clients</a><a href="#" class="footer-link ondark">Team</a>
-            <a href="#" class="footer-link ondark">Offices</a><a href="#" class="footer-link ondark">Careers</a></div>
+            <h5 class="footer-heading"><a href="#" class="no-underline">Our Services</a></h5>
+            <a href="#" class="footer-link ondark">Event Management</a>
+            <a href="#" class="footer-link ondark">Digital Marketing</a>
+            <a href="#" class="footer-link ondark">Talents</a>
+            </div>
         </div>
-        <div class="col-2 lg-3 sm-6 xs-12">
+
+        <div class="col-2 lg-2">
           <div class="footer-links-container">
-            <h5 class="footer-heading"><a href="#" class="no-underline">Let&#x27;s Meet</a></h5>
-            <h5 class="footer-heading"><a href="#" class="no-underline">Hong Kong Office</a></h5>
-            <p class="footer-paragraph">Flat 601, 6/F, Pacific Trade Centre, 2 Kai Hing Road, Kowloon Bay, Hong Kong </p>
-            <p class="footer-paragraph">(852) 2117-1116</p>
-            
+          <h5 class="footer-heading"><a href="#" class="no-underline">Hong Kong Office</a></h5>
+          <p class="footer-paragraph">Flat 601, <Br> 6/F, Pacific Trade Centre, <br> 2 Kai Hing Road, Kowloon Bay, <br>Hong Kong </p>
+          <p class="footer-paragraph">(+852) 2117-1116</p>
+          </div>
+        </div>
+
+        <div class="col-2 lg-2">
+          <div class="footer-links-container">
             <h5 class="footer-heading"><a href="#" class="no-underline">China Office</a></h5>
-            <p class="footer-paragraph"> Rm 602, 11 Xingang Xi Road, Fuli Qianxi Commercial Building, Guangzhou</p>
-            <p class="footer-paragraph">(86) 020-34527167</p>
+            <p class="footer-paragraph"> Rm 602, <br>11 Xingang Xi Road, <br>Fuli Qianxi Commercial Building, <br>Guangzhou, <br>Greater Bay Area, RPC</p>
+            <p class="footer-paragraph">(+86) 020-34527167</p>
             
           </div>
           <div class="negative-margin">
-            <a target="_blank" href="#" class="social-media-link w-inline-block">
-              <div class="fa-icon ondark"></div>
-            </a><a target="_blank" href="#" class="footer-link w-inline-block"></a><a target="_blank" href="#" class="footer-link w-inline-block"></a><a target="_blank" href="#" class="footer-link w-inline-block"></a><a target="_blank" href="#" class="footer-link w-inline-block"></a><a target="_blank" href="#" class="footer-link w-inline-block"></a>
-            <a target="_blank" href="#" class="social-media-link w-inline-block">
-              <div class="fa-icon ondark"></div>
-            </a>
-            <a target="_blank" href="#" class="social-media-link w-inline-block">
-              <div class="fa-icon ondark"></div>
-            </a>
-            <a target="_blank" href="#" class="social-media-link w-inline-block">
-              <div class="fa-icon ondark"></div>
-            </a>
-            <a target="_blank" href="#" class="social-media-link w-inline-block">
-              <div class="fa-icon ondark"></div>
-            </a>
+          <a target="_blank" href="https://www.facebook.com/starlabelgroup" class="social-media-link w-inline-block">
+            <div class="fa-icon ondark"></div>
+          </a>
+          <a target="_blank" href="https://www.youtube.com/channel/UCWuMAxDDXYiZu3drc-fHXwA" class="social-media-link w-inline-block">
+           <div class="fa-icon ondark"></div>
+          </a>
+            
           </div>
         </div>
         <div class="col-2 lg-2 md-4"></div>
