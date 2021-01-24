8818c2e7799cf296e4bb5c92d0b56f96b3236120
Data Ng <data@connect.hku.hk>
2021-01-25 05:16:02 +0800

    update about and event management

--- menu.js
+++ menu.js
@@ -1,7 +1,7 @@
 document.write(`
 <div data-collapse="medium" data-animation="default" data-duration="400" class="nav-bar w-nav">
     <div class="wrapper navbar-2 w-container">
-      <div class="logo-area"><a href="#" class="nav-logo w-inline-block"><img src="images/duduk.svg" width="200" alt=""></a></div>
+      <div class="logo-area"><a href="index.html" class="nav-logo w-inline-block"><img src="images/duduk.svg" width="200" alt=""></a></div>
 	  <nav role="navigation" class="nav-menu-2 w-nav-menu">
 	  <a href="index.html" class="nav-link w-nav-link w--current ">Home</a>
 	  <a href="about-us.html" class="nav-link w-nav-link">About Us</a>
